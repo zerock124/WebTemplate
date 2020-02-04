2679123f33e936e1ac91bd24ea55f6325b0b4ebc
xinyu0124 <xinyu@sp88.com.tw>
2020-02-04 11:51:59 +0800

    身分驗證

---
+++ webTemplate/App_Start/IdnetityConfig.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using webTemplate.Models;
+using static webTemplate.Models.ApplicationUser;
+
+namespace webTemplate.App_Start
+{
+    public class EmailService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // 將您的電子郵件服務外掛到這裡以傳送電子郵件。
+            return Task.FromResult(0);
+        }
+    }
+
+    public class SmsService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // 將您的 SMS 服務外掛到這裡以傳送簡訊。
+            return Task.FromResult(0);
+        }
+    }
+
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+
+        public ApplicationUserManager(IUserStore<ApplicationUser> store) : base(store)
+        {
+        }
+
+        public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
+        {
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+
+            //設定 username 驗證邏輯
+            manager.UserValidator = new UserValidator<ApplicationUser>(manager)
+            {
+                AllowOnlyAlphanumericUserNames = false, //允許使用者名稱只有數字
+                //RequireUniqueEmail = true             //電子郵件必須是唯一的 
+            };
+
+            //設定 密碼 驗證邏輯
+            manager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 6,
+                //RequireNonLetterOrDigit = true,   //密碼至少必須有一個非字母或非數字字元 , 必須包含特殊字元
+                RequireDigit = true,                //密碼至少必須有 1 個數字 ('0' 到 '9')。必須包含數字
+                //RequireLowercase = true,          //必須包含小寫字母
+                //RequireUppercase = true,          //必須包含大寫字母
+            };
+
+            // 設定使用者鎖定詳細資料
+            manager.UserLockoutEnabledByDefault = true;
+            manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            manager.MaxFailedAccessAttemptsBeforeLockout = 5;
+
+            // 註冊雙因素驗證提供者。此應用程式使用手機和電子郵件接收驗證碼以驗證使用者
+            // 您可以撰寫專屬提供者，並將它外掛到這裡。
+            manager.RegisterTwoFactorProvider("電話代碼", new PhoneNumberTokenProvider<ApplicationUser>
+            {
+                MessageFormat = "您的安全碼為 {0}"
+            });
+            manager.RegisterTwoFactorProvider("電子郵件代碼", new EmailTokenProvider<ApplicationUser>
+            {
+                Subject = "安全碼",
+                BodyFormat = "您的安全碼為 {0}"
+            });
+            manager.EmailService = new EmailService();
+            manager.SmsService = new SmsService();
+            var dataProtectionProvider = options.DataProtectionProvider;
+            if (dataProtectionProvider != null)
+            {
+                manager.UserTokenProvider =
+                    new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
+            }
+
+            return manager;
+        }
+    }
+}
