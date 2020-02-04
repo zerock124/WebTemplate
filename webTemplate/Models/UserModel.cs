abd1cf3a4e30b6218c8eb19cf00807dbfebd99dd
xinyu0124 <xinyu@sp88.com.tw>
2020-02-04 15:09:15 +0800

    補充檔案

--- webTemplate/Models/UserModel.cs
+++ webTemplate/Models/UserModel.cs
@@ -9,26 +9,53 @@ using System.Web;
 
 namespace webTemplate.Models
 {
+    // 您可將更多屬性新增至 ApplicationUser 類別，藉此為使用者新增設定檔資料，如需深入了解，請瀏覽 https://go.microsoft.com/fwlink/?LinkID=317594。
     public class ApplicationUser : IdentityUser
     {
-
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
+            // 注意 authenticationType 必須符合 CookieAuthenticationOptions.AuthenticationType 中定義的項目
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // 在這裡新增自訂使用者宣告
             return userIdentity;
         }
 
-        public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-        {
-            public ApplicationDbContext() : base("WebTemplateTestEntities", throwIfV1Schema: false) 
-            {
-            }
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string RealName { get; set; }
 
-            public static ApplicationDbContext Create() 
-            {
-                return new ApplicationDbContext();
-            }
+        /// <summary>
+        /// 註冊日期
+        /// </summary>
+        public DateTime RegisterDate { get; set; }
+
+        /// <summary>
+        /// 建立時間
+        /// </summary>
+        public DateTime CreateTime { get; set; }
+
+        /// <summary>
+        /// 建立人員ID
+        /// </summary>
+        public string CreateUser { get; set; }
+
+        /// <summary>
+        /// 更新時間
+        /// </summary>
+        public DateTime UpdateTime { get; set; }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext() : base("WebTemplateTestEntities", throwIfV1Schema: false)
+        {
         }
 
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
     }
+
 }
